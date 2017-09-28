c96e979fc7fd98d366795e15b115596788c94e18
horowitzb <horowitzb@WITPC0EF3SM.wit.private>
2017-09-28 18:17:07 -0400

    starting point

---
+++ Data_Structures_GroupApp2/src/ExpressionParser.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class ExpressionParser {
+	
+	private ArrayList<String> Expressions;
+	public ExpressionParser() {
+		//takes file
+	}
+	
+	public ArrayList<String> getExpressions() {
+		return Expressions;
+		
+	}
+	
+	public String getExpressionAt(int i) {
+		return null;
+		
+	}
+	
+	public static ArrayList<String> StringToArray(String s){
+		return null;
+		
+	}
+	
+	public String toString() {
+		return null;
+		
+	}
+	
+	
+}
