b3c55b53cc7ea376d6de9a187a8009850cc0adf7
leslievongphakdy <vongphakdyl@wit.edu>
2017-10-04 19:40:48 -0400

    Scan for file

--- Data_Structures_GroupApp2/src/Main.java
+++ Data_Structures_GroupApp2/src/Main.java
@@ -1,9 +1,31 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		try 
+		{
+			System.out.print("Enter file name here: ");
+			
+			Scanner input = new Scanner(System.in);
+			
+			input = new Scanner(file);
+			
+			while (input.hasNextline())
+			{
+				String line = input.nextInt();
+				System.out.println(line);
+			}
+			input.close();
+		}
+	catch (FileNotFoundException e)
+		{
+		e.printStackTrace();
+		}
 	}
-
 }
