3c0907fead91233994844f2ec3b9b126fd61a97f
leslievongphakdy <vongphakdyl@wit.edu>
2017-10-08 20:11:28 -0400

    Does this thing work???

--- Data_Structures_GroupApp2/src/Main.java
+++ Data_Structures_GroupApp2/src/Main.java
@@ -1,17 +1,22 @@
-
+import java.io.BufferedReader;
 
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ExpressionParser ep = new ExpressionParser();
-		System.out.print(ep.toString());
-		
-		testCalculator(ep);
-	}//end main
-	
-	private static void testCalculator(ExpressionParser ep){
-		System.out.println("\nTesting Calculator");
+        	String current;
+        	BufferedReader in = new BufferedReader( new InputStreamReader( System.in ) );
 	}
-		
+	
+	try {
+		System.out.println("Enter txt file name: ");
+		while((current = in.readLine()) != null) {
+			
+			System.out.prinln("Answers: " + current);
+			Calculator calc = new Calculator(current);
+			ExpressionParser ep = new ExpressionParser(current);
+			System.out.println(calc.getValue());
+		}
+	}
+}
