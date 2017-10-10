23af70846b7d7050243385727fd72b2b3b953ab4
Aiden Wells <wellsa1@wit.edu>
2017-10-09 22:04:13 -0400

    formatting fixes

--- Data_Structures_GroupApp2/src/Main.java
+++ Data_Structures_GroupApp2/src/Main.java
@@ -4,33 +4,34 @@
 // Avery Loftin
 // 10/9/2017
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.util.Scanner;
+import javafx.application.Application ;
+import javafx.fxml.FXMLLoader ;
+import javafx.scene.Parent ;
+import javafx.scene.Scene ;
+import javafx.scene.layout.Pane ;
+import javafx.stage.Stage ;
+import java.io.FileNotFoundException ;
+import java.io.FileReader ;
+import java.util.Scanner ;
 
 /**
- * main class to run a gui and solve the invalid multi digit file with the calculator class
+ * Main class to run a gui and solve the invalid multi digit file with the calculator class
  * @author Avery Loftin
  */
-public class Main extends Application{
+public class Main extends Application
+{
 
 	/**
 	 * Main main method
 	 * @param args main method args
 	 */
-	public static void main(String[] args)
+	public static void main( String[] args )
 	{
-		String file = System.getProperty("user.dir") + "/Infix Calculator Expressions - multi-digit with invalid expressions -- 2016-10-04 01.txt";
+		String file = System.getProperty( "user.dir" ) + "/Infix Calculator Expressions - multi-digit with invalid expressions -- 2016-10-04 01.txt" ;
 
-		solveFile(file);
+		solveFile( file ) ;
 
-		launch(args);
+		launch( args ) ;
 	}// end main
 
 
@@ -38,28 +39,28 @@ public class Main extends Application{
 	 * Solves every equation in a file
 	 * @param fileName a file with one equation per line with no spaces and only whole numbers, +, -, *, /, (, and )
 	 */
-	private static void solveFile(String fileName)
+	private static void solveFile( String fileName )
 	{
 		try
 		{
-			Scanner fileScanner = new Scanner(new FileReader(fileName));
-			Calculator calculator = new Calculator();
-			while (fileScanner.hasNextLine())
+			Scanner fileScanner =	new Scanner( new FileReader( fileName ) ) ;
+			Calculator calculator = new Calculator() ;
+			while ( fileScanner.hasNextLine() )
 			{
-				calculator.setEquationString(fileScanner.nextLine());
-				if (calculator.getResult().equals("java.lang.ArithmeticException: / by zero"))
+				calculator.setEquationString( fileScanner.nextLine() ) ;
+				if ( calculator.getResult().equals( "java.lang.ArithmeticException: / by zero" ) )
 				{
-					System.out.println(calculator.getEquationString() + " = " + "Error: Cannot divide by zero");
+					System.out.println( calculator.getEquationString() + " = " + "Error: Cannot divide by zero" ) ;
 				} // end if
 				else
 				{
-					System.out.println(calculator.toString());
+					System.out.println( calculator.toString() ) ;
 				} // end else
 			} // end while
 		} // end try
-		catch (FileNotFoundException fileNotFound)
+		catch ( FileNotFoundException fileNotFound )
 		{
-			System.out.println("File " + fileName + " not found!");
+			System.out.println( "File " + fileName + " not found!" ) ;
 		} // end catch
 	} // end solveFile
 
@@ -69,20 +70,21 @@ public class Main extends Application{
 	 * @param primaryStage the primaryStage to start and show
 	 */
 	@Override
-	public void start(Stage primaryStage)
+	public void start( Stage primaryStage )
 	{
-		Parent root = new Pane();
+		Parent root = new Pane() ;
 		try
 		{
-			root = FXMLLoader.load(getClass().getResource("CalculatorGUI.fxml"));
+			root = FXMLLoader.load(getClass().getResource( "CalculatorGUI.fxml" ) ) ;
 		} // end try
-		catch (java.io.IOException e){
-			System.out.print("ERROR");
-			System.exit(0);
+		catch ( java.io.IOException e )
+		{
+			System.out.print( "ERROR" ) ;
+			System.exit( 0 ) ;
 		} // end catch
 
-		primaryStage.setTitle("Calculator");
-		primaryStage.setScene(new Scene(root, 240, 384));
-		primaryStage.show();
+		primaryStage.setTitle( "Calculator" ) ;
+		primaryStage.setScene( new Scene( root, 240, 384 ) ) ;
+		primaryStage.show() ;
 	} // end start
 } // end Main
